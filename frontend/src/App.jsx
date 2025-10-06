4fa654e24922b6a8ae64440ad73f72438f5e4380
Deepa <raideepa938@gmail.com>
2025-10-06 10:42:24 +0530

    modifying admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import UserLayout from "./components/Layout/UserLayout"
 import Home from "./pages/Home"
 import { Toaster } from "sonner"
@@ -21,6 +21,7 @@ import OrderManagement from "./components/Admin/OrderManagement"
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import ProtectedRoute from "./components/Common/ProtectedRoute"
+// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 
 const App = () => {
@@ -54,7 +55,7 @@ const App = () => {
        <Route path="products" element={<ProductManagement />} />
        <Route path="products/:id/edit" element={<EditProductPage />} />
        <Route path="orders" element={<OrderManagement />} />
-       <Route path="shop" element={<AdminHomePage />} />
+       <Route path="shop" element={ <Navigate to="/collections/all" replace />} />
     
     {/* Admin Layout */}
     </Route>
